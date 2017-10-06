716aaf42dd71077f0257cf107f366945a75267b0
Robert Lord <robert@lord.io>
2017-10-05 22:13:34 -0500

    Even faster, better glyph mapping to size

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 function Font (fontJson) {
   this.unitsPerEm = fontJson.unitsPerEm
-  this.glyphs = []
+  this.unicodeMap = {}
   for (let i = 0; i < fontJson.glyphData.length; i++) {
     let glyph = fontJson.glyphData[i]
-    this.glyphs[i] = {
+    let unicodes = glyph.slice(1)
+    let glyphObj = {
       index: i,
       advanceWidth: glyph[0],
-      unicodes: glyph.slice(1)
+      unicodes: unicodes
+    }
+    for (let j = 0; j < unicodes.length; j++) {
+      this.unicodeMap[unicodes[j]] = glyphObj
     }
   }
   this.getGposKerningValue = null // TODO LOAD PROPERLY
@@ -14,36 +18,15 @@ function Font (fontJson) {
 }
 
 Font.prototype.charToGlyphIndex = function (c) {
-  const code = c.charCodeAt(0)
-  const glyphs = this.glyphs
-  if (glyphs) {
-    for (let i = 0; i < glyphs.length; i += 1) {
-      const glyph = glyphs[i]
-      for (let j = 0; j < glyph.unicodes.length; j += 1) {
-        if (glyph.unicodes[j] === code) {
-          return i
-        }
-      }
-    }
-  }
-  return null
+  return this.unicodeMap[code] || null
 }
 
 Font.prototype.stringToGlyphs = function (s, options) {
   options = options || this.defaultRenderOptions
-  // Get glyph indexes
-  const indexes = []
+  const glyphs = new Array(s.length)
   for (let i = 0; i < s.length; i += 1) {
-    const c = s[i]
-    indexes.push(this.charToGlyphIndex(c))
-  }
-  let length = indexes.length
-
-  // convert glyph indexes to glyph objects
-  const glyphs = new Array(length)
-  const notdef = this.glyphs[0]
-  for (let i = 0; i < length; i += 1) {
-    glyphs[i] = this.glyphs[indexes[i]] || notdef
+    const code = s[i].charCodeAt(0)
+    glyphs[i] = this.unicodeMap[code]
   }
   return glyphs
 }
@@ -64,16 +47,17 @@ Font.prototype.defaultRenderOptions = {
   }
 }
 
-Font.prototype.forEachGlyph = function (text, x, y, fontSize, options, callback) {
-  x = x !== undefined ? x : 0
-  y = y !== undefined ? y : 0
+Font.prototype.forEachGlyph = function (text, fontSize, options, callback) {
+  x = 0
   fontSize = fontSize !== undefined ? fontSize : 72
   options = options || this.defaultRenderOptions
   const fontScale = 1 / this.unitsPerEm * fontSize
   const glyphs = this.stringToGlyphs(text, options)
   for (let i = 0; i < glyphs.length; i += 1) {
     const glyph = glyphs[i]
-    callback.call(this, glyph, x, y, fontSize, options)
+    if (!callback.call(this, glyph, x)) {
+      return
+    }
     if (glyph.advanceWidth) {
       x += glyph.advanceWidth * fontScale
     }
@@ -82,18 +66,12 @@ Font.prototype.forEachGlyph = function (text, x, y, fontSize, options, callback)
       const kerningValue = this.getKerningValue(glyph, glyphs[i + 1])
       x += kerningValue * fontScale
     }
-
-    if (options.letterSpacing) {
-      x += options.letterSpacing * fontSize
-    } else if (options.tracking) {
-      x += (options.tracking / 1000) * fontSize
-    }
   }
   return x
 }
 
 Font.prototype.getAdvanceWidth = function (text, fontSize, options) {
-  return this.forEachGlyph(text, 0, 0, fontSize, options, function () {})
+  return this.forEachGlyph(text, fontSize, options, function (g, x) {return true})
 }
 
 module.exports = Font
--- test.js
+++ test.js
@@ -1,7 +1,15 @@
 const Font = require('./index')
 const data = require('./testout')
+require('fs').unlinkSync('./testout.js')
 
 let f = new Font(data)
+let w = f.getAdvanceWidth("text", 10)
+if (!w) {
+  console.log('Size not returned')
+  process.exit(1)
+}
+if (w < 17.42 || w > 17.43) {
+  console.log('Incorrect size detected: expected ~17.42, got ', w)
+  process.exit(1)
+}
 
-console.log(f.getAdvanceWidth("text", 10))
-require('fs').unlinkSync('./testout.js')
