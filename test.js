a0fb55c606e51b8a0bfc08440beda7781df1310c
Robert Lord <robert@lord.io>
2017-10-05 21:48:37 -0500

    More testing stuff

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const Font = require('./index')
-const data = require('./foobar')
+const data = require('./testout')
 
 let f = new Font(data)
 
 console.log(f.getAdvanceWidth("text", 10))
+require('fs').unlinkSync('./testout.js')
